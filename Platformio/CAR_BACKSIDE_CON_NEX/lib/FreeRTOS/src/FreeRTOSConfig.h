97d054886092ba94f68f82697e39fca5cbbe5109
Golowa70 <Golowa70@gmail.com>
2020-12-28 22:55:23 +0200

    Update FreeRTOSConfig.h

--- Platformio/CAR_BACKSIDE_CON_NEX/lib/FreeRTOS/src/FreeRTOSConfig.h
+++ Platformio/CAR_BACKSIDE_CON_NEX/lib/FreeRTOS/src/FreeRTOSConfig.h
@@ -54,7 +54,7 @@
 #define configMAX_PRIORITIES                4
 #define configMINIMAL_STACK_SIZE            ( 192 )                    //   ( 192 )
 #define configMAX_TASK_NAME_LEN             ( 8 )
-#define configUSE_TRACE_FACILITY            0
+#define configUSE_TRACE_FACILITY            1
 #define configUSE_16_BIT_TICKS              1
 #define configIDLE_SHOULD_YIELD             1
 
